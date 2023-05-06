f856898360d7135235bff77ac7c0bba1021f161f
tuan3440 <tuannguyen190499@gmail.com>
2023-05-07 00:44:45 +0700

    Initial version of School_BE generated by generator-jhipster@7.9.3

---
+++ src/test/java/com/viettel/tuandz/IntegrationTest.java
@@ -0,0 +1,22 @@
+package com.viettel.tuandz;
+
+import com.viettel.tuandz.SchoolBeApp;
+import com.viettel.tuandz.config.AsyncSyncConfiguration;
+import com.viettel.tuandz.config.EmbeddedSQL;
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.annotation.DirtiesContext;
+
+/**
+ * Base composite annotation for integration tests.
+ */
+@Target(ElementType.TYPE)
+@Retention(RetentionPolicy.RUNTIME)
+@SpringBootTest(classes = { SchoolBeApp.class, AsyncSyncConfiguration.class })
+@EmbeddedSQL
+@DirtiesContext(classMode = DirtiesContext.ClassMode.AFTER_CLASS)
+public @interface IntegrationTest {
+}
